0a950daa18aab06bed753c62806695cffd4e27bd
ChrisHilborne <chris.hilborne@gmail.com>
2021-03-09 23:09:19 +0100

    Implemented InfoPage with translations and links to GitHub repos

--- src/Pages/InfoPage.js
+++ src/Pages/InfoPage.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 const InfoPage = () => {
-
+    const { t } = useTranslation("about");
 
     return (
         <>
             <Container>
-                <p>
-                    Info Page
-                </p>
+                <h3>{t('title')}</h3>
+                <p>{t('p1')}<a className="text-link" href="https://www.comunidad.madrid/gobierno/datos-abiertos">{t('link1')}</a>{t('p2')}</p>
+                <p>{t('p3')}</p>
+                <p>{t('p4')}{t('frontend')}<a className="text-link" href="https://github.com/ChrisHilborne/Madrid-Covid-Radar/">{t('here')}</a>{t('backend')}<a className="text-link" href="https://github.com/ChrisHilborne/Madrid-Covid-Radar-Frontend/">{t('here')}</a></p>
+                <p>{t('contact')}<a className="text-link" href="/contact">{t('here')}</a></p>
             </Container>
         </>
     );
